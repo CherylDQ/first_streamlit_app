ebf519a7fa08e79896ee589d4a4d160feb4d9f74
CherylDQ <133910376+CherylDQ@users.noreply.github.com>
2023-05-17 15:32:26 -0500

    Update streamlit_app.py
    Changed Breakfast header from Breakfast Menu to Breakfast Favorites
    Changes spacing of text items
    Added emojis to 3 text items
    Added 1 menu item: Avocado Toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,8 @@ import streamlit
 
 streamlit.title( 'My parents new healthy diner')
 
-streamlit.header( 'Breakfast Menu')
-
-streamlit.text( 'Omega 3 & Blueberry Oatmeal' )
-
-streamlit.text('Kale, Spiniach & Rocket Smoothie')
-
-streamlit.text('Hard-Boiled Free-Ranged Egg')
+streamlit.header( 'Breakfast Favorites')
+  streamlit.text( ' 🥣 Omega 3 & Blueberry Oatmeal' )
+  streamlit.text(' 🥗 Kale, Spiniach & Rocket Smoothie')
+  streamlit.text(' 🐔 Hard-Boiled Free-Ranged Egg')
+  streamlit.text(' 🥑🍞 Avocado Toast')
